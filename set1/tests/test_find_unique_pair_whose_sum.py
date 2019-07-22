ed7fa69fc435b8a0f0ec30475d5faef56e29f502
Sreedhar Bukya <sreedhar.bukya@toppr.com>
2019-07-22 22:45:15 +0800

    fixing the negative value input

--- set1/tests/test_find_unique_pair_whose_sum.py
+++ set1/tests/test_find_unique_pair_whose_sum.py
@@ -3,7 +3,7 @@ from unittest import TestCase
 from set1.unique_pair_who_sum_is_k import find_unique_pair_whose_sum
 
 
-class TestFind_unique_pair_whose_sum(TestCase):
+class Test_Find_unique_pair_whose_sum(TestCase):
 
     def test_find_unique_pair_whose_sum(self):
         input_1 = [1, 2, 3, 4, 5, 6, 7]
@@ -19,3 +19,20 @@ class TestFind_unique_pair_whose_sum(TestCase):
 
         self.assertEqual([[3, 7], [4, 6]], actual_result)
         self.assertEqual(count, 2)
+
+    def test_to_find_empty_input(self):
+        input_1 = []
+        sum = 1
+        actual_result, count = find_unique_pair_whose_sum(input_1, sum)
+
+        self.assertEqual([], actual_result)
+        self.assertEqual(count, 0)
+
+    def test_to_find_negative_values_input(self):
+        input_1 = [1, -2, 12, 3, 4, 4, 5, 6, 6, 7]
+        sum = 10
+        actual_result, count = find_unique_pair_whose_sum(input_1, sum)
+
+        self.assertEqual([[-2, 12], [3, 7], [4, 6]], actual_result)
+        self.assertEqual(count, 2)
+
